8062a038e9639257e97f53de295b3f999f34ecbe
nscherf <nscherf@googlemail.com>
2017-09-23 10:28:54 +0200

    example geomteric transform

---
+++ results/standard-view-transforms/140609-e012-53-standardTransform.m
@@ -0,0 +1,6 @@
+(* Created with the Wolfram Language : www.wolfram.com *)
+TransformationFunction[{{(-1 + Sqrt[3])/(4*Sqrt[2]) + (1 + Sqrt[3])/4, 
+   (-1 - Sqrt[3])/4 + (-1 + Sqrt[3])/(4*Sqrt[2]), (1 - Sqrt[3])/4, 0}, 
+  {(1 - Sqrt[3])/4 + (1 + Sqrt[3])/(4*Sqrt[2]), 
+   (-1 + Sqrt[3])/4 + (1 + Sqrt[3])/(4*Sqrt[2]), (-1 - Sqrt[3])/4, 0}, 
+  {1/2, 1/2, 1/Sqrt[2], 0}, {0, 0, 0, 1}}]
